724d48ef587233a01824b803b9d833b6183a8de1
Mayumi06 <juliamayyumii@gmail.com>
2022-04-27 10:39:31 -0300

    bnd

---
+++ aula5/aula7/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv = "X-UA-Compatible" content="IE=edge">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1"> 
+        <title>Menu Principal</title>
+        <link rel= "stylesheet" href="estilo.css">
+    </head>
+
+    <body>
+        <h1>Menu Principal</h1>
+        <div id ="menu">
+            <ul>
+                <li><a href="cadastrar_usuario.html">Cadastrar Usuário</a></li>
+                <li><a href="listrar_usuario.php">Listar Usuários</a></li>
+            </ul>
+        </div>
+    </body>
+    </head>
+</html>
